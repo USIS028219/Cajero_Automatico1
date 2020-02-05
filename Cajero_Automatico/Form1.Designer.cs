139b1c49afe286f554efdcda90b7dae15e8355a4
KevinRivera717 <k.r.rivera99@gmail.com>
2020-02-05 14:24:05 -0600

    Pagar

--- Cajero_Automatico/Form1.Designer.cs
+++ Cajero_Automatico/Form1.Designer.cs
@@ -32,11 +32,15 @@
             this.LblCantidad = new System.Windows.Forms.Label();
             this.LblRespuesta = new System.Windows.Forms.Label();
             this.TxtCantidad = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // BtnRetirar
             // 
-            this.BtnRetirar.Location = new System.Drawing.Point(62, 110);
+            this.BtnRetirar.Location = new System.Drawing.Point(83, 78);
             this.BtnRetirar.Name = "BtnRetirar";
             this.BtnRetirar.Size = new System.Drawing.Size(75, 23);
             this.BtnRetirar.TabIndex = 0;
@@ -69,11 +73,49 @@
             this.TxtCantidad.Size = new System.Drawing.Size(100, 20);
             this.TxtCantidad.TabIndex = 3;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "PAGAR";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Cantidad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Respuesta: ?";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(299, 261);
+            this.ClientSize = new System.Drawing.Size(382, 271);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.TxtCantidad);
             this.Controls.Add(this.LblRespuesta);
             this.Controls.Add(this.LblCantidad);
@@ -94,6 +136,10 @@
         private System.Windows.Forms.Label LblCantidad;
         private System.Windows.Forms.Label LblRespuesta;
         private System.Windows.Forms.TextBox TxtCantidad;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
